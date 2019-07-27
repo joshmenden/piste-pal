42fbf541fd309cfbef40b70bd4656b05dfd2c66d
Josh Menden <josh.menden@gmail.com>
2019-07-26 23:31:34 -0600

    Progress parsing .gpx into trackpoint objects

--- lib/piste_pal/day_pass.rb
+++ lib/piste_pal/day_pass.rb
@@ -8,7 +8,7 @@ require 'byebug'
 module PistePal
   class DayPass
 
-    attr_accessor :resort_name, :date
+    attr_accessor :resort_name, :date, :trackpoints
 
     def self.purchase file_source
       new(file_source: file_source)
@@ -33,7 +33,24 @@ module PistePal
         @date, @resort_name = name_node.children.first.text.strip.split(" - ")
         @date = Time.parse(@date)
 
+        test_array = []
         trackpoints = doc.xpath("//xmlns:trkpt")
+        trackpoints.each do |tp|
+          params = Hash.new
+          params[:lat] = tp.xpath('@lat').to_s.to_f
+          params[:lon] = tp.xpath('@lon').to_s.to_f
+          tp.children.each do |child|
+            params[:elevation] = child.text.strip.to_f if child.name == "ele"
+            params[:time] = child.text.strip if child.name == "time"
+            params[:hdop] = child.text.strip.to_i if child.name == "hdop"
+            params[:vdop] = child.text.strip.to_i if child.name == "vdop"
+          end
+          extensions = tp.children.search("extensions").first
+          extensions.children.each do |child|
+            params[:speed] = child.attributes["speed"].value.to_s.to_f if child.name == "gps" && !child.attributes["speed"].nil?
+          end
+          test_array.push(params)
+        end
         byebug
         puts "Success parse"
       end
--- lib/piste_pal/trackpoint.rb
+++ lib/piste_pal/trackpoint.rb
@@ -1,10 +1,10 @@
 module PistePal
   class Trackpoint
-    attr_accessor :lat, :long, :elevation, :time, :hdop, :vdop, :speed
+    attr_accessor :lat, :lon, :elevation, :time, :hdop, :vdop, :speed
 
-    def initialize(lat: nil, long: nil, elevation: nil, time: nil, hdop: nil, vdop: nil, speed: nil)
+    def initialize(lat: nil, lon: nil, elevation: nil, time: nil, hdop: nil, vdop: nil, speed: nil)
       @lat = lat
-      @long = long
+      @lon = long
       @elevation = elevation
       @time = time
       @hdop = hdop
